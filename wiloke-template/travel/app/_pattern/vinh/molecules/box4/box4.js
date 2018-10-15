d779ddf2931b51927a70d5ac2a85a153b41099d5
vinh tran <tranvinh@hotmail.com.vn>
2018-10-15 16:00:29 +0700

    psospspsps

--- wiloke-template/travel/app/_pattern/vinh/molecules/box4/box4.js
+++ wiloke-template/travel/app/_pattern/vinh/molecules/box4/box4.js
@@ -1,7 +1,7 @@
 function box4() {
 	const css = {"module":".vinh_box4__module","title":".vinh_box4__title","image":".vinh_box4__image","content":".vinh_box4__content","heading":".vinh_box4__heading","desc":".vinh_box4__desc","thumbList":".vinh_box4__thumbList","small":".vinh_box4__small","headingsm":".vinh_box4__headingsm","":""};
 	;
-	// const thisModule = css.module;
+	const thisModule = css.module;
 	// console.log(thisModule);
 	const items = [...document.querySelectorAll(thisModule)];
 	// items.forEach((item) => item.clientWidth > 320 ? item.wilAddClass('small') : console.log(item));
