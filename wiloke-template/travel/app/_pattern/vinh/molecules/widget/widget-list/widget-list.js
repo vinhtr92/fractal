58ddfb63a91d20e8030f05b56de4eb03e1a0149e
vinh tran <tranvinh@hotmail.com.vn>
2018-12-22 14:50:44 +0700

    update code

---
+++ wiloke-template/travel/app/_pattern/vinh/molecules/widget/widget-list/widget-list.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('footer .widget_list li.has-child > ul').hide();
+
+	$('footer .widget_list li.has-child > a').click(function(event){
+		this.parentNode.setAttribute('data-state',this.parentNode.getAttribute('data-state') === 'open' ? 'closed' :'open')
+		$(this).next().toggle(150)
+		event.preventDefault();
+	})	
+})
+
